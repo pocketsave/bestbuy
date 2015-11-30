5389b41e1ad5bb509edee8440291b13a99a9070a
SANT954 <santu954@gmail.com>
2015-12-01 03:06:30 +0530

    Create BestBuyCalculator.php

---
+++ BestBuyCalculator.php
@@ -0,0 +1,19 @@
+<?php
+interface BestBuyCalculator{
+// This is the top level interface for BestBuy Calculations
+}
+
+interface BB_PriceCalculator extends BestBuyCalculator{
+	//This is meant for lowest price calculations.Includes discounts.
+	function _compareForLeast(...$prices);
+	function _applyDiscount_ComparePrices(...$prices);
+}
+
+interface BB_QualityCalculator extends BestBuyCalculator{
+	//This is meant for quality calculation based on user reviews.
+}
+
+interface BB_ProductCalculator extends BestBuyCalculator{
+	//Overall best sale calculator. This calculates based on best features on BB_PriceCalculator and BB_QualityCalculator.
+}
+?>
